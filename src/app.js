34108d3883ed77d50902d5e5864a66bb8a8188bf
Guillermo Obando <guillermoj.obando@gmail.com>
2024-09-03 19:24:00 +0000

    Functions Domain Random

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function getRandomString(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateDomain() {
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let randomDomain = "";
+
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let g = 0; g < adjs.length; g++) {
+      for (let u = 0; u < nouns.length; u++) {
+        let pronoun = getRandomString(pronouns);
+        let adj = getRandomString(adjs);
+        let noun = getRandomString(nouns);
+
+        randomDomain = pronoun.concat(adj, noun, ".com");
+        console.log(randomDomain);
+      }
+    }
+  }
+}
+generateDomain();
